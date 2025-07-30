1fe458fd6bf11ad5fe68647b7f51ca9549deec3a
Simon Xu <simon.xu@workstream.is>
2025-07-30 11:10:15 +0800

    update

--- lib/worker.dart
+++ lib/worker.dart
@@ -4,18 +4,31 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'providers/app_location.r.dart';
 import 'providers/location_provider.r.dart';
 
+
+class LocationName extends HookConsumerWidget {
+  const LocationName({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final location = ref.watch(locationProvider);
+    return location.when(
+      data: (location) => Text(location),
+      loading: () => const CircularProgressIndicator(),
+      error: (error, stackTrace) => Text('Error: $error'),
+    );
+  }
+}
+
 class WorkerWidget extends HookConsumerWidget {
   const WorkerWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appLocation = ref.watch(appLocationProvider);
-    final location = ref.watch(locationProvider).valueOrNull;
     return appLocation.when(
       data: (appLocation) => Row(
         spacing: 12,
         children: [
-          Text('$location'),  
+          LocationName(),  
           Text('${appLocation['timezone']}'),
         ],
       ),
